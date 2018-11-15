c81b4b5177a7c4023a159a6bbb6b9a8a97ac9d5e
Alejandro <alesitonightray@gmail.com>
2018-11-15 13:29:48 +0100

    Ejercicio 12 terminado

--- src/boletin1/ejercicio12/Ejercicio12.java
+++ src/boletin1/ejercicio12/Ejercicio12.java
@@ -1,5 +1,30 @@
 package boletin1.ejercicio12;
 
-public class Ejercicio12 {
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        GestionXML gestorXML = new GestionXML();
+        InputSource fileXML = new InputSource("C:\\zProyectos\\AccesoADatos\\src\\boletin1\\ejercicio12\\contact.xml");
+        SAXParserFactory saxParserFactory = SAXParserFactory.newDefaultInstance();
+        try {
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            saxParser.parse(new File("C:\\zProyectos\\AccesoADatos\\src\\boletin1\\ejercicio12\\contact.xml"), gestorXML);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/boletin1/ejercicio12/GestionXML.java
@@ -0,0 +1,93 @@
+package boletin1.ejercicio12;
+
+import boletin1.ejercicio08.object.Contact;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GestionXML extends DefaultHandler {
+
+    String locality, prefix, coinType, valor;
+    File f = new File("C:\\zProyectos\\AccesoADatos\\src\\boletin1\\ejercicio12", "ficherin.txt");
+    boolean salto = false;
+    FileWriter wr = null;
+
+    public GestionXML() {
+        super();
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            wr = new FileWriter("C:\\zProyectos\\AccesoADatos\\src\\boletin1\\ejercicio12\\ficherin.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        try {
+            wr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if (!qName.equals("ListContact") && !qName.equals("Contact")) {
+            locality = attributes.getValue("locality");
+            prefix = attributes.getValue("prefix");
+            coinType = attributes.getValue("coinType");
+            salto = false;
+        }
+        if (salto) {
+            try {
+                wr.write("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        String escrito = "";
+        
+        if (!qName.equals("ListContact") && !qName.equals("Contact")) {
+            if (qName.equals("address") && locality != null) {
+                escrito = String.format("locality: %s %s: %s ", locality, qName, valor);
+            } else if (prefix != null) {
+                escrito = String.format("prefix: %s %s: %s ", prefix, qName, valor);
+            } else if (coinType != null) {
+                escrito = String.format("coin type: %s: %s %s", qName, valor, coinType);
+            } else if (qName.equals("debt")){
+                escrito = String.format("%s: %s ", qName, valor.equals("true") ? "si" : "no");
+            } else {
+                escrito = String.format("%s: %s ", qName, valor);
+            }
+
+            try {
+                wr.write(escrito);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            salto = true;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        valor = new String(ch, start, length);
+    }
+}
